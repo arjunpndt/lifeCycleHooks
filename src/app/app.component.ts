b7075ed7d6bfc46524bab43e72e3d3295550c3e0
Arjun Pandit <arjunpandit0840@gmail.com>
2024-01-15 20:20:00 +0530

    Directive Added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,12 @@ import { Component, DoCheck, OnInit } from '@angular/core';
 export class AppComponent implements OnInit, DoCheck {
   title = 'Angular LifeCycle Hooks';
   inputVal = '';
-
+  user: string[] = ['user', 'user2'];
   // OnIinit = false;
 
-  constructor() {}
+  constructor() {
+    var user: string[] = [];
+  }
 
   ngOnInit() {
     console.log('OnInit called');
@@ -22,12 +24,24 @@ export class AppComponent implements OnInit, DoCheck {
   ngAfterContentInit() {
     console.log('AfterContentInit Called');
   }
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
 
   changevalue(value: string): void {
     this.title = value;
   }
   checkHooks(inputValue: HTMLInputElement): void {
     this.inputVal = inputValue.value;
-    // console.log('button clicked', inputValue);
+    this.user.push(inputValue.value);
+    console.log(this.user);
   }
 }
